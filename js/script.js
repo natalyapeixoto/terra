8631b3969085c9f2c7b7f41af34c9badeb4bb507
Jessiane Leal <jessiane.leal@hotmail.com>
2018-10-23 17:07:45 -0300

    Desenvolvimento das seções com informações da empresa, impacto, nav e footer.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $('.carousel').carousel({
     interval: 2500
   })
+
+const bgNav =  document.querySelector(".nav__bg__at");
+
+window.addEventListener("scroll", function(){
+  console.log(pageYOffset)
+  if (pageYOffset >= 400) {
+    bgNav.style.backgroundColor ="white";
+  }
+  if (pageYOffset < 400){
+    bgNav.style.backgroundColor ="rgba(255, 255, 255, 0.466)";
+  }
+    
+})
