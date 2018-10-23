49b47bcbb45cb8a8a6ba9a97da86c8079351ed7e
Natalya <natalya_peixoto@hotmail.com>
2018-10-23 13:13:10 -0300

    working brigade

--- script/index.js
+++ script/index.js
@@ -1,12 +1,9 @@
 const bgNav =  document.querySelector(".nav__bg__at");
-
 window.addEventListener("scroll", function(){
-  console.log(pageYOffset)
   if (pageYOffset >= 400) {
     bgNav.style.backgroundColor ="white";
   }
   if (pageYOffset < 400){
     bgNav.style.backgroundColor ="rgba(255, 255, 255, 0.466)";
-  }
-    
+  } 
 })
