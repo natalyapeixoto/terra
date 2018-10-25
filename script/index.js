1857fd3ffffa80d6e73bebf75c25180ba3c25c9c
Natalya <natalya_peixoto@hotmail.com>
2018-10-25 17:17:08 -0300

    navbar opacity e boxshadow

--- script/index.js
+++ script/index.js
@@ -6,6 +6,7 @@ window.addEventListener("scroll", function(){
   }
   if (pageYOffset < 400){
     bgNav.style.backgroundColor ="rgba(255, 255, 255, 0.466)";
+    bgNav.style.boxShadow = "0px 0px 1px grey";
   } 
 })
 
